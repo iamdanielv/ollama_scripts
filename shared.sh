878b3c4107a99b06d740b721d855c0928b2f909b
iamdanielv <1767914+iamdanielv@users.noreply.github.com>
2025-08-02 00:29:02 -0700

    style: Change colors for displaying Model size
    - model sizes now use background colors to better distinguish between different GB ranges
    - modified date is now shown in magenta, gray doesn't work in some terminal color schemes

--- shared.sh
+++ shared.sh
@@ -740,20 +740,20 @@ print_ollama_models_table() {
     local i=1
 
     while IFS=$'\t' read -r name size_gb modified; do
-        # Determine color for size based on GB.
+        # Determine background color for size based on GB.
         # < 3GB: Green (Small), 3-6GB: Blue (Medium), 6-9GB: Yellow (Large), >= 9GB: Red (Extra Large)
-        local size_color="${C_L_GREEN}" # Default to Small
+        local size_bg_color="${BG_GREEN}" # Default to Small
         local size_gb_int=${size_gb%.*}
 
         if [[ "$size_gb_int" -ge 9 ]]; then
-            size_color="${C_L_RED}"      # Extra Large
+            size_bg_color="${BG_RED}"      # Extra Large
         elif [[ "$size_gb_int" -ge 6 ]]; then
-            size_color="${C_L_YELLOW}"   # Large
+            size_bg_color="${BG_YELLOW}"   # Large
         elif [[ "$size_gb_int" -ge 3 ]]; then
-            size_color="${C_L_BLUE}"     # Medium
+            size_bg_color="${BG_BLUE}"     # Medium
         fi
 
-        printf "  %-5s ${T_BOLD}%-40s${T_RESET} ${size_color}%10s${T_RESET}  ${C_GRAY}%-15s${T_RESET}\n" "$i" "$name" "${size_gb} GB" "$modified"
+        printf "  %-5s ${C_L_CYAN}%-40s${T_RESET} ${size_bg_color}${C_BLACK}%10s${T_RESET}  ${C_MAGENTA}%-15s${T_RESET}\n" "$i" "$name" "${size_gb} GB" "$modified"
         ((i++))
     done < <(_parse_models_to_tsv "$models_json")
 
